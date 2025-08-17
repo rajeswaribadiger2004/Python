00336314d4b4887780528e3ac0d74f73e0e0f06d
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-17 22:08:43 +0530

    Create module_2.py

---
+++ module_2.py
@@ -0,0 +1,41 @@
+1. Write a python program to add two numbers. 
+2. Write a python program to find remainder when a number is divided by z. 
+3. Check the type of variable assigned using input () function. 
+4. Use comparison operator to find out whether ‘a’ given variable a is greater than 
+‘b’ or not. Take a = 34 and b = 80 
+5. Write a python program to find an average of two numbers entered by the user. 
+6. Write a python program to calculate the square of a number entered by the user.
+
+
+1.
+  # Adding two numbers
+a = 5
+b = 7
+sum_result = a + b
+print("The sum is:", sum_result)
+
+2.
+# Finding remainder
+num = 27
+z = 5
+remainder = num % z
+print("The remainder is:", remainder)
+
+3.
+# Checking type of input variable
+x = input("Enter something: ")
+print("You entered:", x)
+print("Type of variable is:", type(x))
+
+4.
+# Average of two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+average = (num1 + num2) / 2
+print("The average is:", average)
+
+5.
+# Square of a number
+num = int(input("Enter a number: "))
+square = num ** 2
+print("The square is:", square)
