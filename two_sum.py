b39f0ec3cd80a46c9e5edd16bc0fa427ee69a2cd
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-23 19:03:26 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,8 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        x = len(nums)
-        for i in range(x-1):
-            for j in range(1,x):
-                if i == j:
-                    continue
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
